bf12a952c807070b1c44cd54d50f45ac5654393f
Jonmarc Jackson <jjackson49@myseneca.ca>
2024-09-29 20:52:30 +0000

    Updated main.tf

--- Terraform_code/dev/main.tf
+++ Terraform_code/dev/main.tf
@@ -43,7 +43,7 @@ locals {
 
 # Retrieve global variables from the Terraform module
 module "globalvars" {
-  source = "../../modules/globalvars"
+  source = "../modules/globalvars"
 }
 
 # Reference subnet provisioned by 01-Networking 
